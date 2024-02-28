02be6b6888ae2cab0adeede5219ee0cc4a6b5d26
loureto <loureto.tads123@gmail.com>
2024-02-27 23:12:20 -0400

    feat: adicionando home page

--- src/modules/home/pages/home.page.tsx
+++ src/modules/home/pages/home.page.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { Button } from "@/components";
 import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components";
+import LogoIcon from "@/public/icons/logo.svg";
 
 export const HomePage = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-[#5BC046] to-[#F2FF56]">
-      <h1 className="mb-24 text-white text-[2.125rem]">Kalkuli</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-br from-40% from-[#5BC046] to-[#F2FF56]">
+      <Image className="mb-24" src={LogoIcon} alt="Kalkuli" />
+
       <section className="flex flex-col max-w-80 text-center gap-6">
-        <p className="text-base font-medium text-white">
+        <p className="text-base leading-[27px] font-medium text-[#D0FFC5]">
           Seja bem-vindo a inteligência especialista em contabilidade.
         </p>
-        <Button>Entrar</Button>
-        <Link href="/" className="text-white text-sm font-medium">
+
+        <Button className="text-[#5BC046] bg-white text-medium">Entrar</Button>
+        <Link href="/home" className="text-white text-sm font-medium">
           Cadastrar
         </Link>
       </section>
