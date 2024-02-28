8b46ad5a38efd34f1c5086727dc723c3a3eee73f
loureto <loureto.tads123@gmail.com>
2024-02-28 00:01:13 -0400

    feat: ajustando o botao

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,42 +1,44 @@
 import { forwardRef } from "react";
-import { useButton, Ripple, Spinner } from "@nextui-org/react";
+import { useButton, Ripple, Spinner, ButtonProps } from "@nextui-org/react";
 import clsx from "clsx";
 
-export const MyButton = forwardRef<HTMLButtonElement, any>((props, ref) => {
-  const {
-    domRef,
-    children,
-    spinnerSize,
-    spinner = <Spinner color="current" size={spinnerSize} />,
-    spinnerPlacement,
-    startContent,
-    endContent,
-    isLoading,
-    disableRipple,
-    styles,
-    getButtonProps,
-    getRippleProps,
-  } = useButton({
-    ref,
-    ...props,
-  });
+export const MyButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const {
+      domRef,
+      children,
+      spinnerSize,
+      spinner = <Spinner color="current" size={spinnerSize} />,
+      spinnerPlacement,
+      startContent,
+      endContent,
+      isLoading,
+      disableRipple,
+      styles,
+      getButtonProps,
+      getRippleProps,
+    } = useButton({
+      ref,
+      ...props,
+    });
 
-  const { ripples } = getRippleProps();
+    const { ripples } = getRippleProps();
 
-  return (
-    <button
-      ref={domRef}
-      className={clsx(styles, props.className)}
-      {...getButtonProps()}
-    >
-      {startContent}
-      {isLoading && spinnerPlacement === "start" && spinner}
-      {children}
-      {isLoading && spinnerPlacement === "end" && spinner}
-      {endContent}
-      {!disableRipple && <Ripple {...getRippleProps()} ripples={ripples} />}
-    </button>
-  );
-});
+    return (
+      <button
+        ref={domRef}
+        className={clsx(styles, props.className)}
+        {...getButtonProps()}
+      >
+        {startContent}
+        {isLoading && spinnerPlacement === "start" && spinner}
+        {children}
+        {isLoading && spinnerPlacement === "end" && spinner}
+        {endContent}
+        {!disableRipple && <Ripple {...getRippleProps()} ripples={ripples} />}
+      </button>
+    );
+  }
+);
 
 MyButton.displayName = "MyButton";
