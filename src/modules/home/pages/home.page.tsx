90c1eb40b2c7f187ca87b8e3495a4109c276357a
loureto <loureto.tads123@gmail.com>
2024-02-28 00:01:27 -0400

    refactor: ajustes na home page

--- src/modules/home/pages/home.page.tsx
+++ src/modules/home/pages/home.page.tsx
@@ -1,26 +1,37 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
-
+import { Link } from "@nextui-org/react";
 import { Button } from "@/components";
+import { routes } from "@/constants";
 import LogoIcon from "@/public/icons/logo.svg";
+import { useHome } from "../hooks";
 
 export const HomePage = () => {
+  const { handleClickNavitgate } = useHome();
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-br from-40% from-[#5BC046] to-[#F2FF56]">
+    <main className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-br from-40% from-custom-accentGreen to-custom-lightYellow">
       <Image className="mb-24" src={LogoIcon} alt="Kalkuli" />
 
-      <section className="flex flex-col max-w-80 text-center gap-6">
-        <p className="text-base leading-[27px] font-medium text-[#D0FFC5]">
+      <section className="flex flex-col max-w-80 items-center gap-6">
+        <p className="text-base leading-[27px] text-center font-medium text-custom-brightGreen">
           Seja bem-vindo a inteligência especialista em contabilidade.
         </p>
 
-        <Button className="text-[#5BC046] bg-white text-medium">Entrar</Button>
-        <Link href="/home" className="text-white text-sm font-medium">
+        <Button
+          className="text-[#5BC046] bg-white w-full text-medium"
+          onClick={handleClickNavitgate}
+        >
+          Entrar
+        </Button>
+
+        <Link
+          href={routes.createAccount}
+          className="text-white w-fit text-sm font-medium"
+        >
           Cadastrar
         </Link>
       </section>
-    </div>
+    </main>
   );
 };
