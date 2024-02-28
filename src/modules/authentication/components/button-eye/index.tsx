471034a9620ce09bfced5f81febfa29ba81c0095
joao victor <loureto.tads123@gmail.com>
2024-02-28 16:53:00 -0400

    feat: adicionando o button eye password

--- src/modules/authentication/components/button-eye/index.tsx
+++ src/modules/authentication/components/button-eye/index.tsx
@@ -10,9 +10,11 @@ interface IButtonEye {
 export const ButtonEye = ({ isVisible, handleClick }: IButtonEye) => {
   return (
     <button className="focus:outline-none" type="button" onClick={handleClick}>
-      {isVisible && <MdRemoveRedEye className="text-gray-500 cursor-pointer" />}
+      {isVisible && (
+        <MdRemoveRedEye className="w-5 h-5 text-gray-500 cursor-pointer" />
+      )}
       {!isVisible && (
-        <MdVisibilityOff className="text-gray-500 cursor-pointer" />
+        <MdVisibilityOff className="w-5 h-5 text-gray-500 cursor-pointer" />
       )}
     </button>
   );
