79ff9eae5896dd3857ce9841b43c48800e6805b4
joao victor <loureto.tads123@gmail.com>
2024-02-28 12:05:43 -0400

    refactor: ajustes no form login

--- src/modules/authentication/components/forms/form-login.tsx
+++ src/modules/authentication/components/forms/form-login.tsx
@@ -1,9 +1,40 @@
 "use client";
 
+import { Button } from "@/components";
+import { Input, Link } from "@nextui-org/react";
+import { useState } from "react";
+import { ButtonEye, Icons } from "..";
+
 export const FormLogin = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   return (
-    <div>
-      <h1>FormLogin</h1>
-    </div>
+    <form className="flex flex-col gap-5">
+      <Input
+        type="email"
+        size="sm"
+        placeholder="E-mail"
+        variant="bordered"
+        isInvalid={true}
+        startContent={<Icons.MdEmail />}
+        // errorMessage="Error message"
+      />
+      <Input
+        type={isVisible ? "text" : "password"}
+        size="sm"
+        placeholder="Senha"
+        variant="bordered"
+        isInvalid={true}
+        startContent={<Icons.MdLock />}
+        endContent={
+          <ButtonEye
+            isVisible={isVisible}
+            handleClick={() => setIsVisible(!isVisible)}
+          />
+        }
+        // errorMessage="Error message"
+      />
+      <Link className="text-center">Esqueceu a senha?</Link>
+      <Button type="submit">Entrar</Button>
+    </form>
   );
 };
