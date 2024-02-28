63ba16b2d0b9dd147d539f72f1bffa32d9ad2a42
loureto <loureto.tads123@gmail.com>
2024-02-27 23:58:54 -0400

    chore: configurando  novas cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,26 @@ const config = {
     "./src/**/*.{ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        custom: {
+          dark: "#202123",
+          darkBlue: "#5536DA",
+          darkGreen: "#37732A",
+          bright: "#444654",
+          brightGray: "#ECECF1",
+          brightGreen: "#D0FFC5",
+          grayIcons: "#C5C5D1",
+          grayFootprint: "#9A9B9F",
+          lightGreen: "#9DD990",
+          accentGreen: "#5BC046",
+          middle: "#343541",
+          lightYellow: "#F2FF56",
+        },
+      },
+    },
+  },
   plugins: [nextui()],
 } satisfies Config;
 
