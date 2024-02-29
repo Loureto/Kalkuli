028b73842e879c53d22f186321f58f51574e68d8
joao victor <loureto.tads123@gmail.com>
2024-02-29 16:36:47 -0400

    chore: adicionando constantes

---
+++ src/modules/authentication/shared/constants/create-account.ts
@@ -0,0 +1,13 @@
+export const createAccountFields = [
+  { id: 1, name: "name", type: "text", placeholder: "Nome" },
+  { id: 2, name: "email", type: "email", placeholder: "E-mail" },
+  { id: 3, name: "cpf", type: "text", placeholder: "CPF" },
+  { id: 4, name: "password", type: "password", placeholder: "Senha" },
+];
+
+export const createAccountJuridicalFields = [
+  { id: 1, name: "company", type: "text", placeholder: "Nome empresa" },
+  { id: 2, name: "email", type: "email", placeholder: "E-mail" },
+  { id: 3, name: "cnpj", type: "text", placeholder: "CNPJ" },
+  { id: 4, name: "password", type: "password", placeholder: "Senha" },
+];
