b4663a58018d65891347cfd7ba005798cfaa0a29
joao victor <loureto.tads123@gmail.com>
2024-02-29 16:27:11 -0400

    refactor: ajustes no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import KalkuliIcon from "@/public/icons/kalkuli-medium.svg";
 
 export const Header = () => {
   return (
-    <header className="w-full flex justify-center py-6 px-5 sm:px-12">
+    <header className="sticky top-0 z-10 w-full flex justify-center bg-white py-6 px-5 sm:px-12">
       <div className="w-full max-w-7xl">
         <Image src={KalkuliIcon} alt="Kalkuli logo" />
       </div>
